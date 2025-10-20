155feba65a7673033ec15232a5086ef649fdd97d
unknown <aryanmishr291@gmail.com>
2025-10-20 19:49:52 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin:[ "https://videoconnect-for-ikarus-3d.onrender.com","http://localhost:3000"],
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     methods: ["GET", "POST"],
     credentials: true
   }
 });
 
 app.use(cors({
-  origin: ["https://videoconnect-for-ikarus-3d.onrender.com" ,"http://localhost:3000"],
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
   credentials: true
 }));
 
